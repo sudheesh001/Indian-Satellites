e44258b4a2a4bcb9f40879ae754f0229759c9f9f
sudheesh001 <sudheesh1995@outlook.com>
2013-07-19 22:22:45 +0530

    Pushing Windows Phone App as open source

---
+++ IndianSatellites/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using IndianSatellites.Resources;
+
+namespace IndianSatellites
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
